8e7cc6a588aa8052d230d990823f0caece9b7bfd
ThrupthiAnn <ThrupthiAnn@users.noreply.github.com>
2020-03-27 23:20:23 +0530

    Ability to save game as gif or video

--- SnakeGame.m
+++ SnakeGame.m
@@ -1,8 +1,12 @@
+clear;
+
 % user options
-nrows = 10; % size of the playing field. It will always be square and even
+nrows = 12; % size of the playing field. It will always be square and even
 AI = @AI_gen12; % choose which agent you want
 create_video = true; % do you want to record the game?
 video_name = 'Snake_game.avi'; % name of the video file for your record
+create_gif = false; % do you want to record the game as a gif? This option sometimes causes the snake to comit suicide, I don't know why
+gif_name = 'Snake_game.gif';
 
 % these variables are not user options
 snake = [1,1];
@@ -11,13 +15,17 @@ score = 0;
 food = SpawnFood(nrows, snake);
 handle = Render(nrows, snake, food);
 data=4;
-if mod(nrows,2)==1
+videoexception = false;
+if mod(nrows,2)==2
     nrows =nrows+1;
 end
 if create_video
     writer = VideoWriter(video_name);
     writer.FrameRate = 25;
     open(writer);
+end
+if create_gif
+    gif_start = true;
 end
 
 % game loop
@@ -32,10 +40,22 @@ while status==1
     if size(snake,1)==nrows*nrows
         break;
     end
-    [handle, frame] = Render(nrows, snake, food,  status, handle);
+    [handle, img] = Render(nrows, snake, food,  status, handle);
     if create_video
-        frame = im2frame(frame);
+        %frame = imresize(frame, 0.4, 'Nearest');
+        frame = im2frame(img);
         writeVideo(writer, frame);
+    end
+    if create_gif
+        simg = imresize(img, 0.2, 'Nearest');
+        simg = im2uint8(simg);
+        [imind, cm] = rgb2ind(img, 256);
+        if gif_start==true
+            imwrite(imind, cm,gif_name, 'gif', 'LoopCount', Inf,'DelayTime',0.001);
+            gif_start = false;
+        else
+            imwrite(imind, cm, gif_name, 'gif', 'WriteMode', 'append', 'DelayTime', 0.001);
+        end
     end
     
     title(sprintf('Score = %d', score));
@@ -44,6 +64,12 @@ catch ME
     if create_video
         close(writer);
     end
+    videoexception = true;
 end
 
+if videoexception==false
+    if create_video
+        close(writer);
+    end
+end
 fprintf('Game over! Final score is %d\n',score);
